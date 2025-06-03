242c72abd1040def63167862e9cb0154311b81d2
Yves Fischer <yvesf+git@xapek.org>
2025-06-03 07:19:37 +0200

    extract PID metrics

--- cmd/ve-ess-shelly/control/control.go
+++ cmd/ve-ess-shelly/control/control.go
@@ -8,30 +8,14 @@ import (
 	"time"
 
 	"github.com/bsm/openmetrics"
-	"github.com/felixge/pidctrl"
 	"github.com/yvesf/ve-ctrl-tool/pkg/timemock"
 )
 
 var (
-	metricControlSetpoint = openmetrics.DefaultRegistry().Gauge(openmetrics.Desc{
-		Name: "ess_pid_setpoint",
-		Unit: "watt",
-		Help: "The current setpoint calculated by the PID controller",
-	})
 	metricControlInput = openmetrics.DefaultRegistry().Gauge(openmetrics.Desc{
 		Name: "ess_pid_input",
 		Unit: "watt",
 		Help: "The current input for the PID controller",
-	})
-	metricControlPIDMin = openmetrics.DefaultRegistry().Gauge(openmetrics.Desc{
-		Name: "ess_pid_output_min",
-		Unit: "watt",
-		Help: "PID min",
-	})
-	metricControlPIDMax = openmetrics.DefaultRegistry().Gauge(openmetrics.Desc{
-		Name: "ess_pid_output_max",
-		Unit: "watt",
-		Help: "PID max",
 	})
 	metricMultiplusSetpoint = openmetrics.DefaultRegistry().Gauge(openmetrics.Desc{
 		Name: "ess_multiplus_setpoint",
@@ -67,7 +51,7 @@ func Run(ctx context.Context, settings Settings, ess ESSControl, meter EnergyMet
 		lastSetpointValue     float64
 	)
 
-	pidC := pidctrl.NewPIDController(0.15, 0.1, 0.15)
+	pidC := NewPIDWithMetrics(0.15, 0.1, 0.15)
 	pidC.SetOutputLimits(-1*float64(settings.MaxWattCharge), float64(settings.MaxWattInverter))
 
 controlLoop:
@@ -83,7 +67,6 @@ controlLoop:
 			slog.Info("no energy meter information", slog.Time("lastMeasurement", lastMeasurement))
 			setpointSet.SetInvalid()
 			metricMultiplusSetpoint.With().Reset(openmetrics.GaugeOptions{})
-			metricControlSetpoint.With().Reset(openmetrics.GaugeOptions{})
 			continue
 		}
 
@@ -105,7 +88,6 @@ controlLoop:
 			controllerOut = 0
 		}
 		setpointSet.Set(ConsumptionPositive(controllerOut))
-		metricControlSetpoint.With().Set(controllerOut)
 
 		// only update the ESS if value is different from last update or 15 seconds passed.
 		// We have to write about every 30s to not let the ESS shutdown for safety reasons.
@@ -132,13 +114,6 @@ controlLoop:
 			metricMultiplusUBat.With().Set(float64(stats.UBat))
 			metricMultiplusInverterPower.With().Set(float64(stats.InverterPower))
 		}
-
-		// set PID output limits.
-		pidC.SetOutputLimits(-1*float64(settings.MaxWattCharge), float64(settings.MaxWattInverter))
-
-		outMin, outMax := pidC.OutputLimits()
-		metricControlPIDMin.With().Set(outMin)
-		metricControlPIDMax.With().Set(outMax)
 	}
 
 	slog.Info("shutdown: reset ESS setpoint to 0")
---
+++ cmd/ve-ess-shelly/control/pid.go
@@ -0,0 +1,48 @@
+package control
+
+import (
+	"time"
+
+	"github.com/bsm/openmetrics"
+	"github.com/felixge/pidctrl"
+)
+
+var (
+	metricControlSetpoint = openmetrics.DefaultRegistry().Gauge(openmetrics.Desc{
+		Name: "ess_pid_setpoint",
+		Unit: "watt",
+		Help: "The current setpoint calculated by the PID controller",
+	})
+	metricControlPIDMin = openmetrics.DefaultRegistry().Gauge(openmetrics.Desc{
+		Name: "ess_pid_output_min",
+		Unit: "watt",
+		Help: "PID min",
+	})
+	metricControlPIDMax = openmetrics.DefaultRegistry().Gauge(openmetrics.Desc{
+		Name: "ess_pid_output_max",
+		Unit: "watt",
+		Help: "PID max",
+	})
+)
+
+type PIDWithMetrics struct {
+	*pidctrl.PIDController
+}
+
+func NewPIDWithMetrics(p, i, d float64) PIDWithMetrics {
+	return PIDWithMetrics{
+		PIDController: pidctrl.NewPIDController(p, i, d),
+	}
+}
+
+func (p PIDWithMetrics) SetOutputLimits(min, max float64) {
+	p.PIDController.SetOutputLimits(min, max)
+	metricControlPIDMin.With().Set(min)
+	metricControlPIDMax.With().Set(max)
+}
+
+func (p PIDWithMetrics) UpdateDuration(value float64, duration time.Duration) float64 {
+	out := p.PIDController.UpdateDuration(value, duration)
+	metricControlSetpoint.With().Set(out)
+	return out
+}
